b305f7699c07184ad4cea43bb638ad00c9d65607
Juan Lopez <anton.lopez09@gmail.com>
2017-07-27 09:05:12 -0400

    fixed audio looping bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Audio from './Audio';
 import playlist from '../songs/playlist.json';
 import marker from '../songs/markers.json';
 
+
 // const App = () => (
 //   <Audio
 //     width={600}
@@ -30,31 +31,33 @@ class App extends React.Component {
     ReactDOM.findDOMNode(this.audioComponent).dispatchEvent(new Event('audio-play'));
   }
   render() {
-    //console.log('Marker obj', marker.markers);
+    // console.log('Marker obj', marker.markers);
     return (
-      <Audio
-        width={600}
-        height={100}
-        fullPlayer={false}
-        comment={true}
-        color="#212121"
-        markers={marker.markers}
-        showMarkers={true}
-        mic={true}
-        recordButton={() => {
-          alert('Hello');
-        }}
-        playlist={playlist.playlist}
-        style={{
-          boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.2)',
-          width: '800px',
-          height: '50px',
-          marginTop: '200px'
-        }}
+      <div>
+        <Audio
+          width={600}
+          height={100}
+          fullPlayer={false}
+          comment={true}
+          color="#212121"
+          markers={marker.markers}
+          showMarkers={true}
+          mic={true}
+          recordButton={() => {
+            alert('Hello');
+          }}
+          playlist={playlist.playlist}
+          style={{
+            boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.2)',
+            width: '800px',
+            height: '50px',
+            marginTop: '200px'
+          }}
 
         // store a reference of the audio component
-        ref={(audioComponent) => { this.audioComponent = audioComponent; }}
-      />
+          ref={(audioComponent) => { this.audioComponent = audioComponent; }}
+        />
+      </div>
     );
   }
 }
