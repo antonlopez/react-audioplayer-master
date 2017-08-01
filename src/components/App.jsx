46e32400b30b10f9f8c77afa62a1bd555c6a012e
mXaln <murmax82@gmail.com>
2017-08-01 14:30:55 -0400

    Added normalization of audio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -76,7 +76,7 @@ class App extends React.Component {
           fullPlayer={false}
           comment={true}
           color="#fff"
-          src="https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3"
+          src="http://172.19.145.91/media/dump/1501176679.73d99dfff8-5117-4635-b734-65140995db67/mrk/07/chapter.wav"
           markers={marker.markers}
           markersButton={true}
           playlist={playlist.playlist}
